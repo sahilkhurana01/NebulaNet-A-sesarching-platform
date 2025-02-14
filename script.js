b3f19e52fd2c1b8143bd639832240190f4a34515
sahilkhurana01 <khuranasahil099@gmail.com>
2025-02-14 22:12:10 +0530

    point 1

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function searchNebulanet(query){
 
 
     const encodedQuery = encodeURIComponent(query)
-    const endpoint = 'https://en.wikipedia.org/w/api.php';
+    const endpoint = 'https://en.wikipedia.org/w/api.php${encodedQuery}';
 
     const response = await fetch(endpoint)
 
@@ -25,3 +25,38 @@ async function searchNebulanet(query){
     return json;
 }
 
+function displayResults(results){
+    //Remove the loading spinner
+    searchResults.innerHTML = ''
+
+    results.forEach((result){
+        const url = `https://en.wikipedia.org/?curid=${results.pageid}`
+
+        const titleLink = '<a href="${url}" target="_blank" rel="noopener"> ${result.title}</a>'
+        const urlLink = '<a href="${url}"  class="result-link" target = "_blank" rel="noopener">${url}</a>'
+
+        const resulTime=document.createElement('div')
+        resulTime.className='result-item'
+        resulTime.innerHTML =`
+        <h3 class="result-title">${titleLink}</h3>
+        ${urlLink}
+        <p class="result-snippet">${result.snippet}</p>
+        `;
+
+        searchResults.appendChild(resulTime);
+
+    })
+}
+
+
+searchForm.addEventListener('submit' , async(e) =>{
+    e.preventDefault()
+
+    const query = searchInput.value.trim()
+    if(!query){
+        searchResults.innerHTML = "<p>Please enter a valid search term.</p>";
+        return;
+    }
+
+    searchResults.innerHTML = "<div class='spinner'>Loading....</div>"
+})
